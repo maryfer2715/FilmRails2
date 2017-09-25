2371a59613d24804628f43dbb271116e08318863
maryfer2715 <mafe_63664@live.com.mx>
2017-09-25 11:17:21 -0500

    guardo

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,2 +1,29 @@
 class FilmsController < ApplicationController
+	def index 
+		@name = "Alejadro y Fernanda "
+	end 
 end
+ def new
+   @film = Film.new
+  end
+
+  def create
+    @film = Film.new films_params
+   if @film.save
+      p "SI SE GUARDO"
+      redirect_to films_path
+    else
+     p "NO SE GUARDO"
+      render :new
+    end
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  private
+  def films_params
+    params.require(:film).permit(:title, :year, :description, :rating)
+   end
+ end
