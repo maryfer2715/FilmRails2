9a5d300fc3cae41a1681dd5cf9d8b2e045f98b20
maryfer2715 <mafe_63664@live.com.mx>
2017-09-27 12:28:31 -0500

    change method users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  # The priority is based upon order of creation: first created -> highest priority.
+
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
  root 'films#index'
   resources :films
+
+  devise_for :users
+  get 'films/index'
 
 
   # You can have the root of your site routed with "root"
---
+++ test/controllers/films_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FilmsControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
